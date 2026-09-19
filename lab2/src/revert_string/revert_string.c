b50c35c2a5c48f7efa4abf74934549060a9f90d2
Gorel0ff <i.gorelow@bk.ru>
2026-09-19 14:39:49 +0300

    Лаба 2 наполовину сделанная

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,10 @@
-#include "revert_string.h"
-
 void RevertString(char *str)
 {
-	// your code here
+	int len = strlen(str);
+    for (int i = 0; i < len / 2; i++) {
+        char temp = str[i];
+        str[i] = str[len - 1 - i];
+        str[len - 1 - i] = temp;
+    }
 }
 
