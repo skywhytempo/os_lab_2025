1bfce6b46dba6bf1e5f56f8213d1358803cf680c
Игорь Горелов <62830403+skywhytempo@users.noreply.github.com>
2026-09-19 15:28:06 +0000

    33333

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,13 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	
+	int len = strlen(str);
+    for (int i = 0; i < len / 2; i++) {
+        char temp = str[i];
+        str[i] = str[len - 1 - i];
+        str[len - 1 - i] = temp;
+    }
 }
 
--- lab2/src/swap/swap.c
+++ lab2/src/swap/swap.c
@@ -2,5 +2,7 @@
 
 void Swap(char *left, char *right)
 {
-
+	char temp = *left;
+	*left = *right;
+	*right = temp;
 }
